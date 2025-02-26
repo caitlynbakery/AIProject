1baf6783e1d8a666caf8e168cf692c9e3ceda4b9
caitlyn <odac@uci.edu>
2025-02-26 10:29:24 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,79 @@
+import openai
+from decouple import config
+from dotenv import load_dotenv, dotenv_values 
+import os
+from IPython.display import Image, display 
+import webbrowser
+
+load_dotenv()
+openai.api_key = os.getenv("SECRET_KEY")
+client = openai.OpenAI(api_key=os.getenv("SECRET_KEY"))
+
+
+class Chatbot:
+    def __init__(self, client):
+        self.client = client
+        self.context = [
+            {"role": "system", "content": "You are a helpful assistant"}
+        ]
+    
+    def chat(self, message):
+        self.context.append(
+            {"role": "user", "content": message}
+        )
+        response = self.client.chat.completions.create(
+            model="gpt-4o",
+            messages=self.context
+        )
+        response_content = response.choices[0].message.content
+        self.context.append({"role": "assistant", "content": response_content})
+        self.print_chat()
+        self.show_car(response_content)
+    
+    def print_chat(self):
+        for message in self.context:
+            if message['role'] == 'user':
+                print("USER: ", message['content'])
+            elif message['role'] == 'assistant':
+                print('RESPONSE: ', message['content'])
+    
+    def show_car(self, message):
+        response = self.client.chat.completions.create(
+            model="gpt-4o",
+            messages=[{"role": "system", "content": """
+                        You are a car image model system. With the model that the user inquires about, generate an image of the car. 
+                    """}, 
+                    {"role": "user", "content": message}]
+        )
+        print(response.choices[0].message.content)
+        image_description = response.choices[0].message.content
+
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=image_description,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        
+        # img = Image(url=response.data[0].url)
+        # output = climage.convert(response.data[0].url) 
+        webbrowser.open(response.data[0].url)
+
+        # prints output on console. 
+        # print(output)
+        # display(Image(url=response.data[0].url))
+
+if __name__ == "__main__":
+    chatbot = Chatbot(client=client)
+    print("Welcome to the friendly chatbot!")
+    print("Input a message to get started.")
+    user_input = input()
+
+    while(user_input != 'stop'):
+        chatbot.chat(user_input)
+        print("Enter stop to exit, or input a new message")
+        user_input = input()
+    
+    
+
